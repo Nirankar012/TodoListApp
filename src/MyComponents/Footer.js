a946e0c20a931640d97d3bfa91c686f41b64414f
nirankar012 <nirankartrivedi.srms@gmail.com>
2021-08-18 11:11:31 +0530

    first commit

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = props => {
+    
+    return (
+        <footer className="bg-dark text-light py-3">
+            <p className="text-center">Copyright &copy; MyTodosList.com</p>
+        </footer>
+    );
+};
+
+export default Footer;
